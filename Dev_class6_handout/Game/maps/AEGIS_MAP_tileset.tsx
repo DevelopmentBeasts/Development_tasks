cf93037b521cd2390fadc50de322f58cec896673
Jaumeavinyo <36154250+Jaumeavinyo@users.noreply.github.com>
2018-10-10 19:21:46 +0200

    todos
    todos

---
+++ Dev_class6_handout/Game/maps/AEGIS_MAP_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="tileset_night_edited_FINAL_VERSION" tilewidth="32" tileheight="32" tilecount="290" columns="29">
+ <image source="../../../../../../Desktop/tiled assets/Magic-Cliffs-Environment/PNG/tileset_night_edited_FINAL_VERSION.png" width="928" height="320"/>
+</tileset>
